46cb2d3d8a25dcd9dbef8fdb053e9a5d28ea6bb1
Yung Tseng <aimystyle3@gmail.com>
2017-10-04 21:52:19 -0400

    Update note.py

--- note.py
+++ note.py
@@ -1,12 +1,12 @@
 
 # print
-print( ">>PRINT" )
+print( "\n>>PRINT" )
 
 print( "Hey pretty!" )
 	# "(text)" or '(text)' doesn't metter
 
 # variables and types
-print( ">>VARIABLES & TYPES" )
+print( "\n>>VARIABLES & TYPES" )
 
 int_1 = 7
 float_1 = 7.0
@@ -32,7 +32,7 @@ a, b = 3, 4
 print( b, a )
 
 # lists
-print( ">>LISTS" )
+print( "\n>>LISTS" )
 
 list_1 = [11, 22, 33]
 print( list_1 )
@@ -46,7 +46,7 @@ list_2.append( "numbers" )
 print(list_2)
 
 # basic operators
-print( ">>BASIC OPERATORS" )
+print( "\n>>BASIC OPERATORS" )
 
 number_1 = 2 + 3 * 4 / 5.0
 print( number_1 )
@@ -67,7 +67,7 @@ print( odd_num + even_num )
 print( odd_num * 3 )
 
 # string formatting
-print( ">>STRING FORMATTING" )
+print( "\n>>STRING FORMATTING" )
 
 name_1 = "Yao"
 age_1 = 24
@@ -80,7 +80,7 @@ print( "List: %s" % list_3 )
 	#any object which is not a string can be formatted using the %s operator
 
 # basic string operations
-print( ">>BASIC STRING OPERATIONS" )
+print( "\n>>BASIC STRING OPERATIONS" )
 
 string_2 = "This is a string!"
 print( len(string_2) )
@@ -95,17 +95,93 @@ print( string_2.startswith("Thi") )	#true
 print( string_2.endswith("Ing!") )	#false
 print( string_2.split("s") )
 
+# conditions
+print( "\n>>CONDITIONS" )
 
+int_2 = 3
+print( int_2 == 3 )
+print( int_2 == 2 )
+print( int_2 < 5 )
 
+name_2 = "Elaina"
+name_3 = ["Elaina", "Bella"]
+age_2 = 24
+if name_2 == "Elaina" and age_2 == 24:
+	print( "This person is now in the US." )
+if name_2 == "Elaina" or name_2 == "Bella":
+	print( "This person's last name is Tseng." )
+if name_2 in name_3:
+	print( "This person's name is either Elaina or Bella." )
 
+time_1 = 1200
+if time_1 > 1200:
+	print( "It's now after noon." )
+elif time_1 < 1200:
+	print( "It's in the morning." )
+else:
+	print( "It's noon now." )
 
+name_4 = "Elaina"
+name_5 = "Elaina"
+name_6 = ["Elaina"]
+name_7 = ["Elaina"]
+print( name_4 == name_5 )
+print( name_4 is name_5 )
+print( name_6 is name_7 )
 
+print( not False )		#true
+print( not False == (True) )	#true
+number_2 = 1
+number_3 = 99
+number_4 = 0
+print( not number_2 )		#false
+print( not number_3 )		#false
+print( not number_4 )		#true
 
+# loops
+print( "\n>>LOOPS" )
 
+for x_1 in range(5):
+	print( x_1 )		#0,1,2,,3,4
+for x_2 in range(3, 6):
+	print( x_2 )		#3,4,5
+for x_3 in range(0, 20, 4):
+	print( x_3 )		#0,4,8,12,16
 
+count_1 = 0
+while count_1 < 3:
+	print( count_1 )	#0,1,2
+	count_1 += 1
 
+count_2 = 0
+while True:
+	print( count_2 )	#0,1,2
+	count_2 += 1
+	if count_2 >= 3:
+		break
+	#break is used to exit a loop (for/while)
 
+for x_4 in range(20):
+	if x_4 % 6 != False:
+		continue
+	print( x_4 )		#0,6,12,18
+	#continue is used to skip the current block
 
+# functions
+print( "\n>>FUNCTIONS" )
+
+def function_1():
+	print( "Hope I can still do functions here lol" )
+function_1()
+
+def function_2( name, favorite_athlete ):
+	print( "%s's favorite athlete is %s" % ( name, favorite_athlete ) )
+function_2( "Yao", "Allen Iverson" )
+
+def multiply_2_numbers( x, y ):
+	return x * y
+x_5 = multiply_2_numbers( 3, 5 )
+print( x_5 )
 
 
 

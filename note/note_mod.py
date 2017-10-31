816f7032fda46e0aa464c8a33002659484c33c66
Yung Tseng <aimystyle3@gmail.com>
2017-10-31 10:31:50 -0400

    Update note_mod.py

--- note/note_mod.py
+++ note/note_mod.py
@@ -1,10 +1,10 @@
 def syntex():
-  print( ">>>import note_mod" )
-  print( ">>>note_mod.syntex" )
-  print( "error happens if do 'note_mod.syntex' before 'import note_mod'" )
+	print( ">>>import note_mod" )
+	print( ">>>note_mod.syntex" )
+	print( "error happens if do 'note_mod.syntex' before 'import note_mod'" )
 
 def func():
-  print( "an empty function" )
+	print( "an empty function" )
 
 def func_2():
-  print( "another empty function" )
+	print( "another empty function" )
